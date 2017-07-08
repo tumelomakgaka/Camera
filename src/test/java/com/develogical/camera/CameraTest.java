9721381294ebbea17e5eaa02391d2404cdd3211f
Ivan Moore <ivan@teamoptimization.com>
2017-07-08 17:56:22 +0100

    use mockito instead

--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -1,18 +1,13 @@
 package com.develogical.camera;
 
-import org.jmock.integration.junit4.JUnitRuleMockery;
-import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.mockito.Mockito.*;
 
 public class CameraTest {
-
-    @Rule
-    public JUnitRuleMockery context = new JUnitRuleMockery();
-
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-
-         // write your test here
+        // write your test here
     }
-
 }
