dcc9429f9b997b34a4b2d91d4b470208456bd7d6
tumelomakgaka <mailing.tumelo@gmail.com>
2018-07-03 17:33:30 +0100

    Camera program changes

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -1,17 +1,43 @@
 package com.develogical.camera;
 
 public class Camera {
+    private Sensor tempSensor;
+    private MemoryCard tempMc;
+    private boolean cameraOn=false;
+    private boolean isCurrentlyWriting=false;
 
-    public void pressShutter() {
-        // not implemented
+    public Camera(Sensor sensor, MemoryCard mc) {
+        tempSensor=sensor;
+        tempMc=mc;
     }
 
+    public void pressShutter(Sensor sensor, MemoryCard mc) {
+        if(cameraOn==true)
+        {
+            isCurrentlyWriting = true;
+            mc.write(sensor.readData(), new WriteCompleteListener() {
+                @Override
+                public void writeComplete() {
+                    isCurrentlyWriting=false;
+
+                }
+            });
+        }
+
+    }
+
+
+
     public void powerOn() {
-        // not implemented
+        tempSensor.powerUp();
+        cameraOn=true;
     }
 
     public void powerOff() {
-       // not implemented
+        if (isCurrentlyWriting == false){
+            tempSensor.powerDown();
+            cameraOn=false;
+        }
     }
 }
 
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -7,7 +7,68 @@ import static org.mockito.Mockito.*;
 
 public class CameraTest {
     @Test
-    public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+    public void TestSwitchingTheCameraOnPowersUpTheSensor() {
+
+        Sensor sensor = mock(Sensor.class);
+        Camera camera = new Camera(sensor, mock(MemoryCard.class));
+        camera.powerOn();
+        verify(sensor).powerUp();
+    }
+
+    @Test
+    public void TestSwitchingTheCameraOffPowersDownTheSensor() {
+
+        Sensor sensor = mock(Sensor.class);
+        Camera camera = new Camera(sensor, mock(MemoryCard.class));
+        camera.powerOff();
+        verify(sensor).powerDown();
+    }
+
+    @Test
+    public void TestCopyDataWhenPressShutterWhilePowerOn() {
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard mc = mock(MemoryCard.class);
+        Camera camera = new Camera(sensor, mc);
+        camera.powerOn();
+        camera.pressShutter(sensor, mc);
+        verify(sensor).readData();
+        verify(mc).write(any(), any());
+    }
+
+    @Test
+    public void TestShutterWhenPowerOff() {
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard mc = mock(MemoryCard.class);
+        Camera camera = new Camera(sensor, mc);
+        camera.powerOff();
+        camera.pressShutter(sensor, mc);
+        verify(sensor, never()).readData();
+        verify(mc, never()).write(any(), any());
+
+    }
+
+    @Test
+    public void doesNotPowerOffSensorUntilFinishedWritingData() {
+        Sensor sensor = mock(Sensor.class);
+        FakeMemoryCard mc = new FakeMemoryCard();
+        Camera camera = new Camera(sensor, mc);
+        camera.powerOn();
+        camera.pressShutter(sensor, mc);
+        camera.powerOff();
+
+        verify(sensor, never()).powerDown();
+
+        mc.writeCompleteListener.writeComplete();
+        camera.powerOff();
+        verify(sensor).powerDown();
+    }
+
+    private static class FakeMemoryCard implements MemoryCard {
+        public WriteCompleteListener writeCompleteListener;
+
+        @Override
+        public void write(byte[] data, WriteCompleteListener writeCompleteListener) {
+            this.writeCompleteListener = writeCompleteListener;
+        }
     }
 }
